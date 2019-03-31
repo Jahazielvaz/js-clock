b513c1ef79385f92d65961d6e17b7be80a0cc148
Jahaziel <jahaziel.vaz@gmail.com>
2019-03-30 21:52:20 -0600

    Working on getting the degrees

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-let currentDate = new Date();
-let currentTime = currentDate.getSeconds();
+// let currentDate = new Date();
+// let currentTime = currentDate.getSeconds();
+//
+// let secondsHand = document.getElementById('seconds-hand');
 
-let secondsHand = document.getElementById('seconds-hand');
+// let secondsRotation = (change) => {
+//   change = 0;
+//    change += 6;
+//    // secondsHand += secondsHand.classList.add("seconds-rotation");
+//    secondsHand.style.transform = `rotate(${change}deg)`
+// }
+//
+//
+//
+// let secondsChanger = () => {
+//     setInterval(secondsRotation, 1000);
+// }
+//
+// secondsChanger()
 
-let secondsRotation = () => {
-   return secondsHand.classList.add("seconds-rotation");
+
+let setDate = () => {
+  const now = new Date();
+  let seconds = now.getSeconds();
+  let secondsDegrees = (seconds / 60) * 360;
+  console.log(secondsDegrees)
 }
 
-let tryFunc = () => {
-    return setInterval(console.log('Im chillin'), 1000);
-}
+setInterval(setDate, 1000);
